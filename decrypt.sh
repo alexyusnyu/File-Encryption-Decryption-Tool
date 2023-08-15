b8f3dadc9f1b0c063588fdf443bd8de134a9edf9
Алекс Ш <slayer1649@gmail.com>
2023-08-15 13:46:12 +0300

    update

--- decrypt.sh
+++ decrypt.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-   echo "Usage: $0 <input_file> <output_file>"
-   exit 1
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
 fi
 
 input_file="$1"
 output_file="$2"
 
 if [ ! -f "$input_file" ]; then
-   echo "Error: Input file '$input_file' not found."
-   exit 1
+    echo "Error: Input file '$input_file' not found."
+    exit 1
 fi
 
 read -s -p "Enter decryption password: " password
@@ -19,8 +19,8 @@ echo
 openssl enc -d -aes-256-cbc -in "$input_file" -out "$output_file" -k "$password"
 
 if [ $? -eq 0 ]; then
-   echo "Decryption successful. Decrypted file: $output_file"
-   rm -f "$input_file"
+    echo "Decryption successful. Decrypted file: $output_file"
+    rm -f "$input_file"
 else
-   echo "Decryption failed."
+    echo "Decryption failed."
 fi
--- encrypt.sh
+++ encrypt.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-   echo "Usage: $0 <input_file> <output_file>"
-   exit 1
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
 fi
 
 input_file="$1"
 output_file="$2"
 
 if [ ! -f "$input_file" ]; then
-   echo "Error: Input file '$input_file' not found."
-   exit 1
+    echo "Error: Input file '$input_file' not found."
+    exit 1
 fi
 
 read -s -p "Enter encryption password: " password
@@ -19,8 +19,8 @@ echo
 openssl enc -aes-256-cbc -salt -in "$input_file" -out "$output_file" -k "$password"
 
 if [ $? -eq 0 ]; then
-   echo "Encryption successful. Encrypted file: $output_file"
-   rm -f "$input_file"
+    echo "Encryption successful. Encrypted file: $output_file"
+    rm -f "$input_file"
 else
-   echo "Encryption failed."
+    echo "Encryption failed."
 fi
